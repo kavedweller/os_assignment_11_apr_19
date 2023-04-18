11413ca48284c0a620548ce68ce88bff9139fc91
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-04-19 02:08:17 +0600

    added: multiplicationTable.js

---
+++ multiplicationTable.js
@@ -0,0 +1,10 @@
+
+function multiplicationTable(n) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(n + " × " + i + " = " + i * n);
+    }
+}
+
+// multiplicationTable(7);
+// console.log('\n');
+// multiplicationTable(99);
